4c7d0aa59a6cb7ca31fa32cba0ed431969c1a781
JohnnyHill11 <johnny.hill1185@gmail.com>
2021-01-10 20:00:33 +0200

    homework#10

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let firstName = document.querySelector('.first-name').value;
-let lastName = document.querySelector('.last-name').value;
-let phoneNumber = document.querySelector('.phone-number').value;
+let firstName = document.querySelector('.first-name');
+let lastName = document.querySelector('.last-name');
+let phoneNumber = document.querySelector('.phone-number');
 let table = document.querySelector('.my-table');
 let btnAdd = document.querySelector('.btn-add');
 btnAdd.addEventListener('click', onBtnAddRow);
@@ -8,39 +8,34 @@ addEventListener('click', onBtnDeleteRow);
 
 function onBtnAddRow() {
   addNewRow();
-  clearInput();
+  clearInputs();
 }
 
 function addNewRow() {
-  validInputs();
-  createNewRow();
-}
-
-function clearInput() {
-  firstName = '';
-  lastName = '';
-  phoneNumber = '';
-}
-
-function validInputs() {
-  if((!firstName || !isNaN(firstName )) || 
-  (!lastName || !isNaN(lastName )) || 
-  (isNaN(phoneNumber) || !phoneNumber)) {
+  if((!firstName.value || !isNaN(firstName.value )) || 
+  (!lastName.value || !isNaN(lastName.value )) || 
+  (!phoneNumber.value || isNaN(phoneNumber.value))) {
     alert("Введите правильное значение!");
     return;
   }
+  createNewRow ();
 }
 
-function creatNewRow() {
+function clearInputs() {
+  firstName.value = '';
+  lastName.value = '';
+  phoneNumber.value = '';
+}
+
+function createNewRow () {
   let newRow = table.insertRow();
   let firstNameCell = newRow.insertCell(0);
   let lastNameCell = newRow.insertCell(1);
   let phoneNumberCell = newRow.insertCell(2);
   let deleteRowCell = newRow.insertCell(3);
-  
-  firstNameCell.innerHTML = firstName;
-  lastNameCell.innerHTML = lastName;
-  phoneNumberCell.innerHTML = phoneNumber;
+  firstNameCell.innerHTML = firstName.value;
+  lastNameCell.innerHTML = lastName.value;
+  phoneNumberCell.innerHTML = phoneNumber.value;
   deleteRowCell.innerHTML = '<button>Удалить</button>';
 }
 
